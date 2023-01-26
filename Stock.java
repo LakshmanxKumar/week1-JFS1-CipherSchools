50b29973b13db60900f48a29d47de6e78dd6fbe8
LakshmanxKumar <122230058+LakshmanxKumar@users.noreply.github.com>
2023-01-26 23:48:17 +0530

    Add files via upload

---
+++ Stock.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class Stock {
+    public class stock {
+        public String one;
+        // Accessible EveryWhere;
+        private String two;
+        //Accessible Only Here;
+        protected  String  three;
+        // Accessible To Derived Class;
+        String four;
+        // Accessible to Same Package;
+    }
+}
