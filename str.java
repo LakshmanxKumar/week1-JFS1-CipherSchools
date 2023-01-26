6a9f3b9b1b19959c85a19596c3e0e2262b22f5c8
LakshmanxKumar <122230058+LakshmanxKumar@users.noreply.github.com>
2023-01-26 23:45:48 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class str {
+    public static void main(String[] args) {
+        char ch='A';
+        while(ch<=90) {
+            System.out.print(ch+ " ");
+            ch++;
+        }
+
+                char ch1='A';
+        do {
+            System.out.print(ch1+" ");
+            ch1++;
+        } while (ch1<='Z');
+    }
+}
